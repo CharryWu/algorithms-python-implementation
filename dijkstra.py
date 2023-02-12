342aba8db192aa8a403c15663794a24d4960babb
Charry Wu <wuzc.charry2013@gmail.com>
2023-02-12 00:21:52 -0800

    Update Dij comment add findSCC

--- dijkstra.py
+++ dijkstra.py
@@ -11,8 +11,11 @@ def calculate_distances(graph, starting_vertex):
     while len(pq) > 0:
         current_distance, current_vertex = heapq.heappop(pq)
 
-        # Nodes can get added to the priority queue multiple times. We only
-        # process a vertex the first time we remove it from the priority queue.
+        # Nodes can get added to the priority queue multiple times. 
+        # This is done to allow negative weight edges (but not cycles) in the graph
+        # If there's neg edges, the algorithm may still execute correctly.
+        # See https://stackoverflow.com/a/6799344 for explanation of dij on graph w/ neg weights
+        # We only process a vertex the first time we remove it from the priority queue.
         if current_distance > distances[current_vertex]:
             continue
 
@@ -40,3 +43,18 @@ example_graph = {
 }
 print(calculate_distances(example_graph, 'X'))
 # => {'U': 1, 'W': 2, 'V': 2, 'Y': 1, 'X': 0, 'Z': 2}
+
+#       A
+#       / \
+#      /   \
+#     /     \
+#   5       2
+#   ↙         ↘
+#   B--(-10)-->C
+example_graph = {
+    'A': {'C': 2, 'B': 5 },
+    'B': {'C': -99},
+    'C': {}
+}
+print(calculate_distances(example_graph, 'A'))
+# => {'A': 0, 'B': 5, 'C': -94}
